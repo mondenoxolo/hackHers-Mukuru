13015360d6291e21111fbce3e1c8620482f536fd
wtc <wtc@pop-os.localdomain>
2024-08-22 15:51:53 +0200

    Zimbabwe

---
+++ src/components/Zimbabwe.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import './Zimstyle.css';
+
+function Zimbabwe() {
+    return (
+        <>
+        <div className="background-container">
+        <div className="content">
+            <h1>
+                Zimbabwe!
+            </h1>
+            <li>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/AFC.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/Atlas.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/CABS.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/CBZ.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/Get_Bucks.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/Mega_save.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/Metro_hyper.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/metro_wholesalers.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/N_Richards_group.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/NMB.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/OK.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/POSB.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/SPAR.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="/src/pictures/Zimbabwe/Zapalala.png" width="400"/>
+                </ul>
+                <ul>
+                    <img src="src/pictures/Zimbabwe/ZB.png" width="400"/>
+                </ul>
+            </li>
+        </div>
+        </div>
+    </>
+    
+    );
+}
+
+export default Zimbabwe;
