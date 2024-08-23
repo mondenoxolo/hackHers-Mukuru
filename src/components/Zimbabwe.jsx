ed842bcfbc02b8a617618d7c749f2135a0305667
wtc <wtc@pop-os.localdomain>
2024-08-23 13:21:12 +0200

    Zim styling

--- src/components/Zimbabwe.jsx
+++ src/components/Zimbabwe.jsx
@@ -1,63 +1,37 @@
 import { useState } from 'react';
 import './Zimstyle.css';
-import Navbar from './Navbar';
 
 function Zimbabwe() {
     return (
         <>
-        
             <div className="background-container">
                 <div className="content">
-                <img src="/src/pictures/Zimbabwe/Money.png" alt="Atlas" />
-                    <h1>Zimbabwe</h1>
-                    <p>Receiving money in Zimbabwe has never been easier. <br></br> For fast and affordable money transfers, choose Mukuru.</p>
-                    <ul className="image-list">
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/AFC.png" alt="AFC" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/Atlas.png" alt="Atlas" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/CABS.png" alt="CABS" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/CBZ.png" alt="CBZ" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/Get_Bucks.png" alt="Get Bucks" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/Mega_save.png" alt="Mega Save" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/Metro_hyper.png" alt="Metro Hyper" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/metro_wholesalers.png" alt="Metro Wholesalers" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/N_Richards_group.png" alt="N Richards Group" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/NMB.png" alt="NMB" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/OK.png" alt="OK" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/POSB.png" alt="POSB" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/SPAR.png" alt="SPAR" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/Zapalala.png" alt="Zapalala" />
-                        </li>
-                        <li>
-                            <img src="/src/pictures/Zimbabwe/ZB.png" alt="ZB" />
-                        </li>
-                    </ul>
+                    <div className="icon-header-container">
+                        <div className="icon-container">
+                            <img src="src/pictures/Zimbabwe/cash_icon.png" alt="Cash Icon" />
+                        </div>
+                        <h1>Zimbabwe</h1>
+                    </div>
+                    <p>Receiving money in Zimbabwe has never been easier. <br /> For fast and affordable money transfers, choose Mukuru.</p>
+                    <div className="image-list-container">
+                        <ul className="image-list">
+                            <li><img src="/src/pictures/Zimbabwe/AFC.png" alt="AFC" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/Banc.png" alt="Atlas" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/Cabs.png" alt="CABS" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/CBZ.png" alt="CBZ" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/Get_Bucks.png" alt="Get Bucks" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/Mega_save.png" alt="Mega Save" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/Metro_hyper.png" alt="Metro Hyper" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/m_wholesalers.png" alt="Metro Wholesalers" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/N_Richards_group.png" alt="N Richards Group" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/nmb.png" alt="NMB" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/OK.png" alt="OK" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/POSB.png" alt="POSB" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/SPAR.png" alt="SPAR" /></li>
+                            <li><img src="/src/pictures/Zimbabwe/Zapalala.png" alt="Zapalala" /></li>
+                            <li><img src="src/pictures/Zimbabwe/zb.png" alt="ZB" /></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </>
