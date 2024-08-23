b733076455bd008984e75c596763c6a9763ba97c
wtc <wtc@pop-os.localdomain>
2024-08-23 11:37:14 +0200

    added mlawi page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
 import { useState } from 'react';
 import './App.css';
-<<<<<<< Updated upstream
-// import CountryDropDown from './components/CountryDropDown';
-import LandingPage from './components/LandingPage';
-// import LandingPage from './components/LandingPage';
-<<<<<<< Updated upstream
-import Zambia from './components/Zambia';
-import LandingPage from './components/LandingPage';
 
-=======
->>>>>>> Stashed changes
-=======
-import Malawi from './components/Malawi';
->>>>>>> Stashed changes
+import LandingPage from './components/LandingPage';
+// import Malawi from './components/Malawi';
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-      <LandingPage/>
-=======
       <LandingPage /> 
->>>>>>> Stashed changes
-=======
-      <Malawi/>
->>>>>>> Stashed changes
-    </>
+      </>
+
   );
 }
 
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { useState } from 'react';
 import CountryDropDown from './CountryDropDown';
 import './LandingPage.css';
@@ -112,25 +111,3 @@ function LandingPage() {
 }
 
 export default LandingPage;
-=======
-// import { useState } from 'react';
-
-// function LandingPage() {
-//     return (
-//         <>
-//             <div style={{
-//                 backgroundImage: `url('https://www.mukuru.com/wp-content/uploads/2022/05/Send-money-to-Eswatini-Mukuru.jpg')`,
-//                 backgroundPosition: 'center',
-//                 backgroundSize: 'cover',
-//                 backgroundRepeat: 'no-repeat',
-//                 width: '100vw',
-//                 height: '70vh',
-//                 position: 'relative',
-//             }}>
-//             </div>
-//         </>
-//     );
-// }
-
-// export default LandingPage;
->>>>>>> Stashed changes
