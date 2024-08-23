19e8105fde3e6b651630de0226f27a64e9d88372
wtc <wtc@pop-os.localdomain>
2024-08-23 13:50:16 +0200

    Zambia edited

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import LandingPage from './components/LandingPage';
 import Zimbabwe from './components/Zimbabwe';
 import GenericCountryPage from './components/GenericCountryPage';
+import Zambia from './components/Zambia';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/zimbabwe" element={<Zimbabwe />} />
         <Route path="/lesotho" element={<GenericCountryPage />} />
+        <Route path="/zambia" element={<Zambia />} />
 
 
       </Routes>
