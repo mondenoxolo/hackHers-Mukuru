40b5a77c7d022886dd78a8d024c06771e620a1e9
wtc <wtc@pop-os.localdomain>
2024-08-23 13:41:29 +0200

    Zambia info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/LandingPage';
+import Zambia from './components/Zambia';
 // import LandingPage from './components/LandingPage';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/zimbabwe" element={<Zimbabwe />} />
+        <Route path="/zambia" element={<Zambia />} />
       </Routes>
     </Router>
   );
