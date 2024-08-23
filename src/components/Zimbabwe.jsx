9194e81bc9cdb5d967c70b971549fa8a0a830beb
Nokulunga <nshabangu023@student.wethinkcode.co.za>
2024-08-23 08:56:15 +0200

    icon for zim

--- src/components/Zimbabwe.jsx
+++ src/components/Zimbabwe.jsx
@@ -4,62 +4,61 @@ import './Zimstyle.css';
 function Zimbabwe() {
     return (
         <>
-        <div className="background-container">
-        <div className="content">
-            <h1>
-                Zimbabwe!
-            </h1>
-            <li>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/AFC.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/Atlas.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/CABS.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/CBZ.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/Get_Bucks.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/Mega_save.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/Metro_hyper.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/metro_wholesalers.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/N_Richards_group.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/NMB.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/OK.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/POSB.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/SPAR.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="/src/pictures/Zimbabwe/Zapalala.png" width="400"/>
-                </ul>
-                <ul>
-                    <img src="src/pictures/Zimbabwe/ZB.png" width="400"/>
-                </ul>
-            </li>
-        </div>
-        </div>
-    </>
-    
+            <div className="background-container">
+                <div className="content">
+                <img src="/src/pictures/Zimbabwe/Money.png" alt="Atlas" />
+                    <h1>Zimbabwe</h1>
+                    <p>Receiving money in Zimbabwe has never been easier. <br></br> For fast and affordable money transfers, choose Mukuru.</p>
+                    <ul className="image-list">
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/AFC.png" alt="AFC" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/Atlas.png" alt="Atlas" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/CABS.png" alt="CABS" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/CBZ.png" alt="CBZ" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/Get_Bucks.png" alt="Get Bucks" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/Mega_save.png" alt="Mega Save" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/Metro_hyper.png" alt="Metro Hyper" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/metro_wholesalers.png" alt="Metro Wholesalers" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/N_Richards_group.png" alt="N Richards Group" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/NMB.png" alt="NMB" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/OK.png" alt="OK" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/POSB.png" alt="POSB" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/SPAR.png" alt="SPAR" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/Zapalala.png" alt="Zapalala" />
+                        </li>
+                        <li>
+                            <img src="/src/pictures/Zimbabwe/ZB.png" alt="ZB" />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 }
 
