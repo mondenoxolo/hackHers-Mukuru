354a10862956a8693e5bec0591c21d9eaa032588
wtc <wtc@pop-os.localdomain>
2024-08-23 14:01:23 +0200

    k

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from './components/LandingPage';
 import Zimbabwe from './components/Zimbabwe';
 import GenericCountryPage from './components/GenericCountryPage';
 import Zambia from './components/Zambia';
+import Malawi from'./components/Malawi';
 
 function App() {
   return (
@@ -11,13 +12,13 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/zimbabwe" element={<Zimbabwe />} />
-        {/* <Route path="/lesotho" element={<GenericCountryPage />} />
-        <Route path="/botswana" element={<GenericCountryPage />} />
-        <Route path="/mozambique" element={<GenericCountryPage />} />
+        <Route path="/lesotho" element={<GenericCountryPage />} />
+        {/* <Route path="/botswana" element={<GenericCountryPage />} />
+        // <Route path="/mozambique" element={<GenericCountryPage />} />
         <Route path="/south-africa" element={<GenericCountryPage />} />
-        <Route path="/eswatini" element={<GenericCountryPage />} />
+        <Route path="/eswatini" element={<GenericCountryPage />} /> */}
         <Route path="/zambia" element={<Zambia />} />
-        <Route path="/malawi" element={<Malawi />} /> */}
+        <Route path="/malawi" element={<Malawi />} /> 
 
 
       </Routes>
